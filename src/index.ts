0b8ea1ad4709937c6da3c36ed3e5b2b25b933a77
YeisonAlexander2001 <110367659+YeisonAlexander2001@users.noreply.github.com>
2022-08-10 18:07:53 -0500

    Services and types

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import express from 'express' 
+import diaryRouter from './routes/diaries'
+
 const app = express()
 
 app.use(express.json())
 
 const PORT = 3000  
 
-app.get('/ping',(_req, res) => {console.log('someone pinged here!!')
+app.get('/ping',(_req, res) => {console.log('someone pinged here!!' + new Date().toLocaleDateString())
 res.send('pong')
 })
 
-app.listen(PORT,()=>
-console.log('Server running on por ${PORT}')
-)
+app.use('/api/diaries', diaryRouter)
+
+app.listen(PORT,()=>{
+console.log('Server running on port ${PORT}')
+})
